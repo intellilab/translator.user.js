0284b9c8fe922c48dc2fe33ac7902a9e58a571f4
Gerald <gera2ld@163.com>
2017-04-07 16:03:32 +0800

    load version from package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,25 @@
 import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+
+const pkg = require('./package.json');
 
 export default {
   entry: 'src/translator.user.js',
   dest: 'dist/translator.user.js',
   format: 'iife',
-  plugins: [babel({
-    presets: [
-      ['env', {
-        modules: false,
-        targets: {
-          browsers: ['chrome >= 45'],
-        },
-      }],
-    ],
-  })],
+  plugins: [
+    babel({
+      presets: [
+        ['env', {
+          modules: false,
+          targets: {
+            browsers: ['chrome >= 45'],
+          },
+        }],
+      ],
+    }),
+    replace({
+      VERSION: pkg.version,
+    }),
+  ],
 };
--- src/translator.user.js
+++ src/translator.user.js
@@ -3,7 +3,7 @@
 // @namespace https://lufei.so
 // @supportURL https://github.com/intellilab/translator.user.js
 // @description 划词翻译
-// @version 1.5.5
+// @version VERSION
 // @run-at document-start
 // @grant GM_addStyle
 // @grant GM_xmlhttpRequest
