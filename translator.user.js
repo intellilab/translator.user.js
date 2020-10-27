09d823092aaf6dc371ee23cc2e5bdb48716f8fee
gera2ld <gera2ld@live.com>
2020-10-27 06:16:48 +0000

    Deploying to release from master @ cbc67b827af6b0cc1407f71be94d1901654011f8 🚀

--- translator.user.js
+++ translator.user.js
@@ -3,13 +3,13 @@
 // @namespace https://lufei.so
 // @supportURL https://github.com/intellilab/translator.user.js
 // @description 划词翻译
-// @version 1.6.6
+// @version 1.6.7
 // @run-at document-start
 // @grant GM_addStyle
 // @grant GM_getValue
 // @grant GM_setValue
 // @grant GM_xmlhttpRequest
-// @require https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@1,npm/@violentmonkey/ui@0.3
+// @require https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@1,npm/@violentmonkey/ui@0.4
 // @include *
 // ==/UserScript==
 
@@ -214,8 +214,8 @@ const provider$1 = {
   }
 };
 
-var styles = {"section":"style-module_section__1Eiq1","label":"style-module_label__JD9KX","content":"style-module_content__1MvKK","phonetic":"style-module_phonetic__2SIsx","item":"style-module_item__2QPXK"};
-var stylesheet=":host a{all:initial;color:#7cbef0;cursor:pointer;font-family:inherit;font-size:inherit;line-height:inherit}:host a:hover{text-decoration:underline}:host .style-module_section__1Eiq1{display:flex;align-items:flex-start;font-size:12px;line-height:1.2}:host .style-module_section__1Eiq1:not(:first-child){border-top:1px solid #eee}:host .style-module_label__JD9KX{display:block;margin:8px 8px 8px 0;padding:2px 0;color:#fff;background:#bbb;border-radius:4px;font-size:12px;line-height:1.4;text-transform:uppercase;writing-mode:vertical-rl}:host .style-module_content__1MvKK{flex:1;min-width:0;padding:8px 0}:host .style-module_content__1MvKK>*{display:block}:host .style-module_content__1MvKK>:not(:first-child){margin-top:8px}:host .style-module_phonetic__2SIsx{display:inline-block;margin-left:8px}:host .style-module_item__2QPXK~.style-module_item__2QPXK{margin-top:8px}";
+var styles = {"link":"style-module_link__YVV7m","section":"style-module_section__1Eiq1","block-top":"style-module_block__1NZsy","block-bottom":"style-module_block__1NZsy","label":"style-module_label__JD9KX","content":"style-module_content__1MvKK","phonetic":"style-module_phonetic__2SIsx","item":"style-module_item__2QPXK"};
+var stylesheet=":host .style-module_link__YVV7m{position:relative;color:#7cbef0;cursor:pointer}:host .style-module_link__YVV7m:hover{text-decoration:underline}:host .style-module_section__1Eiq1{display:flex;align-items:flex-start;font-size:12px;line-height:1.2}:host .style-module_section__1Eiq1:not(:first-child){border-top:1px solid #eee}:host .style-module_block__1NZsy{display:block}:host .style-module_label__JD9KX{display:block;margin:8px 8px 8px 0;padding:2px 0;color:#fff;background:#bbb;border-radius:4px;font-size:12px;line-height:1.4;text-transform:uppercase;writing-mode:vertical-rl}:host .style-module_content__1MvKK{flex:1;min-width:0;padding:8px 0}:host .style-module_content__1MvKK>*{display:block}:host .style-module_content__1MvKK>:not(:first-child){margin-top:8px}:host .style-module_phonetic__2SIsx{display:inline-block;margin-left:8px}:host .style-module_item__2QPXK{display:block}:host .style-module_item__2QPXK~.style-module_item__2QPXK{margin-top:8px}";
 
 const React = VM;
 let audio;
@@ -228,10 +228,21 @@ function play(url) {
 }
 
 function getPlayer(url) {
-  return e => {
-    e.preventDefault();
+  return () => {
     play(url);
   };
+}
+
+function handleOpenUrl(e) {
+  const {
+    href
+  } = e.target.dataset;
+  const a = /*#__PURE__*/React.createElement("a", {
+    href: href,
+    target: "_blank",
+    rel: "noopener noreferrer"
+  });
+  a.click();
 }
 
 function render(results, {
@@ -248,31 +259,39 @@ function render(results, {
       explains,
       translations
     } = result;
-    panel.append( /*#__PURE__*/React.createElement("section", {
+    panel.append( /*#__PURE__*/React.createElement(panel.id, {
       className: styles.section
-    }, /*#__PURE__*/React.createElement("div", {
+    }, /*#__PURE__*/React.createElement(panel.id, {
       className: styles.label
-    }, name), /*#__PURE__*/React.createElement("div", {
+    }, name), /*#__PURE__*/React.createElement(panel.id, {
       className: styles.content
-    }, !!(query || (phonetic == null ? void 0 : phonetic.length)) && /*#__PURE__*/React.createElement("div", null, query && /*#__PURE__*/React.createElement("span", null, query), phonetic == null ? void 0 : phonetic.map(({
+    }, !!(query || (phonetic == null ? void 0 : phonetic.length)) && /*#__PURE__*/React.createElement(panel.id, {
+      className: styles.block
+    }, query && /*#__PURE__*/React.createElement(panel.id, null, query), phonetic == null ? void 0 : phonetic.map(({
       html,
       url
-    }) => /*#__PURE__*/React.createElement("a", {
-      className: styles.phonetic,
+    }) => /*#__PURE__*/React.createElement(panel.id, {
+      className: `${styles.phonetic} ${styles.link}`,
       dangerouslySetInnerHTML: {
         __html: html
       },
       onClick: getPlayer(url)
-    }))), explains && /*#__PURE__*/React.createElement("div", null, explains.map(item => /*#__PURE__*/React.createElement("div", {
+    }))), explains && /*#__PURE__*/React.createElement(panel.id, {
+      className: styles.block
+    }, explains.map(item => /*#__PURE__*/React.createElement(panel.id, {
       className: styles.item,
       dangerouslySetInnerHTML: {
         __html: item
       }
-    }))), detailUrl && /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("a", {
-      target: "_blank",
-      rel: "noopener noreferrer",
-      href: detailUrl
-    }, "\u66F4\u591A...")), translations && /*#__PURE__*/React.createElement("div", null, translations.map(item => /*#__PURE__*/React.createElement("div", {
+    }))), detailUrl && /*#__PURE__*/React.createElement(panel.id, {
+      className: styles.block
+    }, /*#__PURE__*/React.createElement(panel.id, {
+      className: styles.link,
+      "data-href": detailUrl,
+      onClick: handleOpenUrl
+    }, "\u66F4\u591A...")), translations && /*#__PURE__*/React.createElement(panel.id, {
+      className: styles.block
+    }, translations.map(item => /*#__PURE__*/React.createElement(panel.id, {
       className: styles.item,
       dangerouslySetInnerHTML: {
         __html: item
