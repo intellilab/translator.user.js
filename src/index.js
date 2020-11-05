17369328001320bc9a6921e9968615d3aa8a6e20
Gerald <gera2ld@live.com>
2020-11-05 17:44:57 +0800

    refactor: add youdao provider module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { request } from './util';
+import { provider as youdaoProvider } from './youdao';
 import { provider as bingProvider } from './bing';
 import { provider as googleProvider } from './google';
 import styles, { stylesheet } from './style.module.css';
@@ -91,57 +91,7 @@ function render(results, { event, panel }) {
 }
 
 const providers = [
-  {
-    name: 'youdao',
-    handle: async (text) => {
-      const payload = {
-        type: 'data',
-        doctype: 'json',
-        version: '1.1',
-        relatedUrl: 'http://fanyi.youdao.com/',
-        keyfrom: 'fanyiweb',
-        key: null,
-        translate: 'on',
-        q: text,
-        ts: Date.now(),
-      };
-      const result = await request({
-        url: 'https://fanyi.youdao.com/openapi.do',
-        params: payload,
-        responseType: 'json',
-      });
-      if (result.errorCode) throw result;
-      const { basic, query, translation } = result;
-      if (basic) {
-        const noPhonetic = '&hearts;';
-        const {
-          explains,
-          'us-phonetic': us,
-          'uk-phonetic': uk,
-        } = basic;
-        return {
-          query,
-          phonetic: [
-            {
-              html: `UK: [${uk || noPhonetic}]`,
-              url: `https://dict.youdao.com/dictvoice?audio=${encodeURIComponent(query)}&type=1`,
-            },
-            {
-              html: `US: [${us || noPhonetic}]`,
-              url: `https://dict.youdao.com/dictvoice?audio=${encodeURIComponent(query)}&type=2`,
-            },
-          ],
-          explains,
-          detailUrl: `http://dict.youdao.com/search?q=${encodeURIComponent(query)}`,
-        };
-      }
-      if (translation?.[0]) {
-        return {
-          translations: translation,
-        };
-      }
-    },
-  },
+  youdaoProvider,
   bingProvider,
   googleProvider,
 ];
---
+++ src/youdao.js
@@ -0,0 +1,53 @@
+import { request } from './util';
+
+export const provider = {
+  name: 'youdao',
+  handle: async (text) => {
+    const payload = {
+      type: 'data',
+      doctype: 'json',
+      version: '1.1',
+      relatedUrl: 'http://fanyi.youdao.com/',
+      keyfrom: 'fanyiweb',
+      key: null,
+      translate: 'on',
+      q: text,
+      ts: Date.now(),
+    };
+    const result = await request({
+      url: 'https://fanyi.youdao.com/openapi.do',
+      params: payload,
+      responseType: 'json',
+    });
+    if (result.errorCode) throw result;
+    const { basic, query, translation } = result;
+    if (basic) {
+      const noPhonetic = '&hearts;';
+      const {
+        explains,
+        'us-phonetic': us,
+        'uk-phonetic': uk,
+      } = basic;
+      return {
+        query,
+        phonetic: [
+          {
+            html: `UK: [${uk || noPhonetic}]`,
+            url: `https://dict.youdao.com/dictvoice?audio=${encodeURIComponent(query)}&type=1`,
+          },
+          {
+            html: `US: [${us || noPhonetic}]`,
+            url: `https://dict.youdao.com/dictvoice?audio=${encodeURIComponent(query)}&type=2`,
+          },
+        ],
+        explains,
+        detailUrl: `http://dict.youdao.com/search?q=${encodeURIComponent(query)}`,
+      };
+    }
+    if (translation?.[0]) {
+      return {
+        translations: translation,
+      };
+    }
+  },
+};
