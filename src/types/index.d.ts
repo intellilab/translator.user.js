73ac14862d63746ee23cc23c7b141a46e9b17a81
Gerald <gera2ld@live.com>
2021-11-30 22:53:44 +0800

    fix: fix JSX return type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,8 +1,13 @@
 import * as dom from '@violentmonkey/dom';
 import * as ui from '@violentmonkey/ui';
+import { VChildren, DomNode } from '@gera2ld/jsx-dom';
 
 declare global {
   const VM: typeof dom & typeof ui;
+
+  namespace JSX {
+    type Element = DomNode;
+  }
 }
 
 interface TranslatorResponse {
